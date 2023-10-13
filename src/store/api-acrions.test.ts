450076b7d3752e5f788f42ecede84eca822680db
Larisa Zubkova <larissazubkova@gmail.com>
2023-10-13 16:17:44 +0300

    Начинает тесты

--- src/store/api-acrions.test.ts
+++ src/store/api-acrions.test.ts
@@ -4,11 +4,48 @@ import thunk from 'redux-thunk';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { State } from '../types/state';
 import { Action } from 'redux';
-import { AppThunkDispatch } from '../utils/mocks';
+import { AppThunkDispatch, extractActionsTypes } from '../utils/mocks';
+import { checkAuthAction } from './api-actions';
+import { APIRoute } from '../const';
 
 describe('Async axtions', () => {
   const axios = createAPI();
   const mockAxiosAdapter = new MockAdapter(axios);
   const middleware = [thunk.withExtraArgument(axios)];
   const mockStoreCreator = configureMockStore<State, Action<string>, AppThunkDispatch>(middleware);
+  let store: ReturnType<typeof mockStoreCreator>;
+
+  beforeEach(() => {
+    store = mockStoreCreator({FILMS: {films: []}});
+  });
+
+  describe('checkAuthAction', () => {
+    it('should dispatch "checkAuthAction.pending" and "checkAuthAction.fulfilled" with thunk "checkAuthAction"', async () => {
+      mockAxiosAdapter.onGet(APIRoute.Login).reply(200);
+      await store.dispatch(checkAuthAction());
+      const actions = extractActionsTypes(store.getActions());
+
+      expect(actions).toEqual([
+        checkAuthAction.pending.type,
+        checkAuthAction.fulfilled.type,
+      ]);
+    });
+
+    it('should dispatch "checkAuthAction.pending" and "checkAuthAction.rejected" when server response 400', async () => {
+      mockAxiosAdapter.onGet(APIRoute.Login).reply(400);
+      await store.dispatch(checkAuthAction());
+      const actions = extractActionsTypes(store.getActions());
+
+      expect(actions).toEqual([
+        checkAuthAction.pending.type,
+        checkAuthAction.rejected.type,
+      ]);
+    });
+  });
+
+  describe('fetchFilmsAction', () => {
+    it('should dispatch "fetchFilmsAction.pending" and "fetchFilmsAction.fulfilled" when server response 200', () => {
+
+    });
+  });
 });
--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -1,6 +1,18 @@
+import { random, system, internet } from 'faker';
 import { ThunkDispatch } from 'redux-thunk';
 import { State } from '../types/state';
 import { createAPI } from '../services/api';
 import { Action } from 'redux';
+import { FilmShortCard } from '../types/film';
 
 export type AppThunkDispatch = ThunkDispatch<State, ReturnType<typeof createAPI>, Action>;
+
+export const extractActionsTypes = (actions: Action<string>[]) => actions.map(({type}) => type);
+
+export const makeFakeFilm = (): FilmShortCard => ({
+  id: crypto.randomUUID(),
+  name: random.words(),
+  previewImage: system.filePath(),
+  previewVideoLink: internet.url(),
+  genre: random.word(),
+});
